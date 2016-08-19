53a3263dd4cd8a93515e2b6f7c37ba675ae72105
Nantha Harish JR <harish-rajendran@users.noreply.github.com>
2016-08-19 12:45:32 +0530

    Add files via upload

---
+++ substring.py
@@ -0,0 +1,22 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Admin
+#
+# Created:     18-08-2016
+# Copyright:   (c) Admin 2016
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    a=raw_input()
+    b=raw_input()
+    if b in a:
+        print "its a substring"
+    else:
+        print "its not a substring"
+
+
+if __name__ == '__main__':
+    main()
